5a4ee8dcf08004b34a6a0fa58354a15bc4f2fe9e
Ilkka Rinne <ilkka.rinne@spatineo.com>
2019-03-21 11:29:23 +0200

    May now be used to parse a single message or a collection (bulletin)

--- src/main/java/fi/fmi/avi/converter/tac/TACParser.java
+++ src/main/java/fi/fmi/avi/converter/tac/TACParser.java
@@ -2,13 +2,13 @@ package fi.fmi.avi.converter.tac;
 
 import fi.fmi.avi.converter.AviMessageSpecificConverter;
 import fi.fmi.avi.converter.tac.lexer.AviMessageLexer;
-import fi.fmi.avi.model.AviationWeatherMessage;
+import fi.fmi.avi.model.AviationWeatherMessageOrCollection;
 
 /**
  *
  * @author Ilkka Rinne / Spatineo Oy 2017
  */
-public interface TACParser<T extends AviationWeatherMessage> extends AviMessageSpecificConverter<String, T> {
+public interface TACParser<T extends AviationWeatherMessageOrCollection> extends AviMessageSpecificConverter<String, T> {
 
     void setTACLexer(AviMessageLexer lexer);
 
