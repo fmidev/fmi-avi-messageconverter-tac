bdd0338051a601e3e3b333417476b47bd358a3de
patrick-alaspaa <patrick.alaspaa@spatineo.com>
2020-10-09 10:36:38 +0300

    Check for string value Nil as first value in remark string, if found do not add as list item

--- src/main/java/fi/fmi/avi/converter/tac/swx/SWXTACParser.java
+++ src/main/java/fi/fmi/avi/converter/tac/swx/SWXTACParser.java
@@ -159,7 +159,7 @@ public class SWXTACParser extends AbstractTACParser<SpaceWeatherAdvisory> {
 
         firstLexeme.findNext(LexemeIdentity.REMARKS_START, (match) -> {
             final List<String> remarks = getRemarks(match, hints);
-            if (!remarks.isEmpty()) {
+            if (!remarks.isEmpty() && !remarks.get(0).equalsIgnoreCase("NIL")) {
                 builder.setRemarks(remarks);
             }
         });
