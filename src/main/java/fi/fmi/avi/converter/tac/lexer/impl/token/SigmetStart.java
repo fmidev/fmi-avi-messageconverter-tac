01485e8fa757df79414e95bea3422c723e5f6b11
Ernst de Vreede <ernst.de.vreede@knmi.nl>
2021-04-06 22:08:50 +0200

    Added Tokenizer code

--- src/main/java/fi/fmi/avi/converter/tac/lexer/impl/token/SigmetStart.java
+++ src/main/java/fi/fmi/avi/converter/tac/lexer/impl/token/SigmetStart.java
@@ -2,9 +2,17 @@ package fi.fmi.avi.converter.tac.lexer.impl.token;
 
 import static fi.fmi.avi.converter.tac.lexer.LexemeIdentity.REAL_SIGMET_START;
 
+import java.util.Optional;
+
 import fi.fmi.avi.converter.ConversionHints;
 import fi.fmi.avi.converter.tac.lexer.Lexeme;
+import fi.fmi.avi.converter.tac.lexer.SerializingException;
+import fi.fmi.avi.converter.tac.lexer.impl.FactoryBasedReconstructor;
 import fi.fmi.avi.converter.tac.lexer.impl.PrioritizedLexemeVisitor;
+import fi.fmi.avi.converter.tac.lexer.impl.ReconstructorContext;
+import fi.fmi.avi.model.AviationWeatherMessageOrCollection;
+import fi.fmi.avi.model.sigmet.SIGMET;
+
 import static fi.fmi.avi.converter.tac.lexer.Lexeme.ParsedValueName.LOCATION_INDICATOR;
 
 /**
@@ -24,4 +32,21 @@ public class SigmetStart extends PrioritizedLexemeVisitor {
             token.setParsedValue(LOCATION_INDICATOR, words[0]);
         }
     }
+    public static class Reconstructor extends FactoryBasedReconstructor {
+
+        @Override
+        public <T extends AviationWeatherMessageOrCollection> Optional<Lexeme> getAsLexeme(final T msg, final Class<T> clz, final ReconstructorContext<T> ctx)
+                throws SerializingException {
+            if (SIGMET.class.isAssignableFrom(clz)) {
+                SIGMET sigmet = (SIGMET)msg;
+                StringBuilder sb=new StringBuilder();
+                sb.append(sigmet.getMeteorologicalWatchOffice());
+                sb.append(" ");
+                sb.append("SIGMET");
+                return Optional.of(createLexeme(sb.toString(), REAL_SIGMET_START));
+            }
+            return Optional.empty();
+        }
+    }
+
 }
