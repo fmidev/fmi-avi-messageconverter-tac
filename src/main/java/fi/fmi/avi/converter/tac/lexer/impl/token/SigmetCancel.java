9f05c92f3299b15ccdb0b41a59e83543e0e3f0fc
Ernst de Vreede <ernst.de.vreede@knmi.nl>
2021-05-03 17:25:31 +0200

    Added parsing of SIGMET Cancel

---
+++ src/main/java/fi/fmi/avi/converter/tac/lexer/impl/token/SigmetCancel.java
@@ -0,0 +1,82 @@
+package fi.fmi.avi.converter.tac.lexer.impl.token;
+
+import fi.fmi.avi.converter.ConversionHints;
+import fi.fmi.avi.converter.tac.lexer.Lexeme;
+import fi.fmi.avi.converter.tac.lexer.SerializingException;
+import fi.fmi.avi.converter.tac.lexer.Lexeme.ParsedValueName;
+import fi.fmi.avi.converter.tac.lexer.impl.FactoryBasedReconstructor;
+import fi.fmi.avi.converter.tac.lexer.impl.ReconstructorContext;
+import fi.fmi.avi.converter.tac.lexer.impl.RegexMatchingLexemeVisitor;
+import fi.fmi.avi.model.AviationWeatherMessageOrCollection;
+import fi.fmi.avi.model.PartialOrCompleteTimeInstant;
+import fi.fmi.avi.model.sigmet.SIGMET;
+import fi.fmi.avi.model.sigmet.SigmetReference;
+
+import java.util.Optional;
+import java.util.regex.Matcher;
+
+import static fi.fmi.avi.converter.tac.lexer.LexemeIdentity.SIGMET_CANCEL;
+
+/**
+ * Created by rinne on 10/02/17.
+ */
+public class SigmetCancel extends RegexMatchingLexemeVisitor {
+
+    public SigmetCancel(final OccurrenceFrequency prio) {
+        super("^CNL SIGMET (\\w?\\d?\\d) (\\d{2})(\\d{2})(\\d{2})/(\\d{2})(\\d{2})(\\d{2})$", prio);
+    }
+
+    @Override
+    public void visitIfMatched(final Lexeme token, final Matcher match, final ConversionHints hints) {
+        token.identify(SIGMET_CANCEL);
+        token.setParsedValue(ParsedValueName.SEQUENCE_DESCRIPTOR, match.group(1));
+        token.setParsedValue(ParsedValueName.DAY1, Integer.valueOf(match.group(2)));
+        token.setParsedValue(ParsedValueName.HOUR1, Integer.valueOf(match.group(3)));
+        token.setParsedValue(ParsedValueName.MINUTE1, Integer.valueOf(match.group(4)));
+        token.setParsedValue(ParsedValueName.DAY2, Integer.valueOf(match.group(5)));
+        token.setParsedValue(ParsedValueName.HOUR2, Integer.valueOf(match.group(6)));
+        token.setParsedValue(ParsedValueName.MINUTE2, Integer.valueOf(match.group(7)));
+    }
+
+	public static class Reconstructor extends FactoryBasedReconstructor {
+
+        @Override
+        public <T extends AviationWeatherMessageOrCollection> Optional<Lexeme> getAsLexeme(final T msg, final Class<T> clz, final ReconstructorContext<T> ctx)
+                throws SerializingException {
+            if (SIGMET.class.isAssignableFrom(clz)) {
+                SIGMET sigmet = (SIGMET)msg;
+                System.err.println("SIGMET Cancel recon: "+ sigmet.isCancelMessage());
+                if (sigmet.isCancelMessage()) {
+                    StringBuilder sb = new StringBuilder();
+                    SigmetReference ref = sigmet.getCancelledReference().get();
+                    // sb.append(ref.getIssuingAirTrafficServicesUnit().getDesignator());
+                    // sb.append(" ");
+                    // sb.append(ref.getIssuingAirTrafficServicesUnit().getName());
+                    // sb.append(" ");
+                    sb.append("CNL");
+                    sb.append(" ");
+                    sb.append("SIGMET");
+                    sb.append(" ");
+                    sb.append(ref.getSequenceNumber());
+                    sb.append(" ");
+                    PartialOrCompleteTimeInstant start = ref.getValidityPeriod().getStartTime().get();
+                    PartialOrCompleteTimeInstant end = ref.getValidityPeriod().getEndTime().get();
+
+
+                    sb.append(String.format("%02d%02d%02d",
+                            start.getDay().getAsInt(),
+                            start.getHour().getAsInt(),
+                            start.getMinute().getAsInt()));
+                    sb.append("/");
+                    sb.append(String.format("%02d%02d%02d",
+                            end.getDay().getAsInt(),
+                            end.getHour().getAsInt(),
+                            end.getMinute().getAsInt()));
+                    System.err.println("SB:"+sb.toString());
+                    return Optional.of(createLexeme(sb.toString(), SIGMET_CANCEL));
+                }
+            }
+            return Optional.empty();
+        }
+    }
+}
---
+++ src/test/java/fi/fmi/avi/converter/tac/sigmet/TestSigmetCancel.java
@@ -0,0 +1,67 @@
+package fi.fmi.avi.converter.tac.sigmet;
+
+import fi.fmi.avi.converter.ConversionHints;
+import fi.fmi.avi.converter.ConversionSpecification;
+import fi.fmi.avi.converter.tac.conf.TACConverter;
+import fi.fmi.avi.converter.tac.lexer.LexemeIdentity;
+import fi.fmi.avi.model.sigmet.SIGMET;
+import fi.fmi.avi.model.sigmet.immutable.SIGMETImpl;
+
+import static fi.fmi.avi.converter.tac.lexer.LexemeIdentity.*;
+
+/**
+ *
+ * TODO:
+ * - OBS_OR_FORECAST is not detecting correctly
+ * - FirType with three words fails (NEW AMSTERDAM FIR)
+ * - Wrong phenomenon is returned (EMB_TS instead of SEV_ICE_FZRA)
+ * - sigmet1a.json is not yet correct ()
+ */
+
+public class TestSigmetCancel extends AbstractAviMessageTestTempSigmet<String, SIGMET> {
+
+	@Override
+	public String getJsonFilename() {
+		return "../sigmet/sigmetcancel.json";
+	}
+
+	@Override
+	public String getMessage() {
+		return "EHAA SIGMET M03 VALID 271330/271530 EHDB-\r\nEHAA AMSTERDAM FIR CNL SIGMET M01 271130/271530=";
+	}
+
+	@Override
+	public ConversionHints getLexerParsingHints() {
+		return ConversionHints.SIGMET;
+	}
+
+	@Override
+	public ConversionHints getParserConversionHints() {
+		return ConversionHints.SIGMET;
+	}
+
+	@Override
+	public LexemeIdentity[] getLexerTokenSequenceIdentity() {
+        return spacify(new LexemeIdentity[] { SIGMET_START, REAL_SIGMET_START,
+				SEQUENCE_DESCRIPTOR, VALID_TIME, MWO_DESIGNATOR, FIR_DESIGNATOR, SIGMET_FIR_NAME_WORD, SIGMET_FIR_NAME_WORD, FIR_NAME,
+				SIGMET_PHENOMENON, OBS_OR_FORECAST, SIGMET_ENTIRE_AREA,
+				SIGMET_MOVING, SIGMET_INTENSITY,
+				END_TOKEN });
+	}
+
+	@Override
+    public ConversionSpecification<String, SIGMET> getParsingSpecification() {
+		return TACConverter.TAC_TO_SIGMET_POJO;
+    }
+
+    @Override
+    public ConversionSpecification<SIGMET, String> getSerializationSpecification() {
+        return TACConverter.SIGMET_POJO_TO_TAC;
+    }
+
+	@Override
+	public Class<? extends SIGMET> getTokenizerImplmentationClass() {
+		return SIGMETImpl.class;
+	}
+
+}
