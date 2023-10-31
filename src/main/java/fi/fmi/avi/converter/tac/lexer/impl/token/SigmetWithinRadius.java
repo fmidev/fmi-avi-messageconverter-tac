b0b19d41339a4c54f59a028999956919afc3d283
kasarit <49980037+kasarit@users.noreply.github.com>
2023-10-31 17:50:08 +0200

    Fix SigmetWithinRadius coordinates

--- src/main/java/fi/fmi/avi/converter/tac/lexer/impl/token/SigmetWithinRadius.java
+++ src/main/java/fi/fmi/avi/converter/tac/lexer/impl/token/SigmetWithinRadius.java
@@ -1,38 +1,28 @@
 package fi.fmi.avi.converter.tac.lexer.impl.token;
 
-import static fi.fmi.avi.converter.tac.lexer.Lexeme.ParsedValueName.RDOACT_RADIUS;
-import static fi.fmi.avi.converter.tac.lexer.Lexeme.ParsedValueName.RDOACT_RADIUS_UNIT;
-import static fi.fmi.avi.converter.tac.lexer.Lexeme.ParsedValueName.RDOACT_LAT;
-import static fi.fmi.avi.converter.tac.lexer.Lexeme.ParsedValueName.RDOACT_LON;
-
 import fi.fmi.avi.converter.ConversionHints;
 import fi.fmi.avi.converter.tac.geoinfo.GeoUtilsTac;
 import fi.fmi.avi.converter.tac.lexer.Lexeme;
-import fi.fmi.avi.converter.tac.lexer.LexemeIdentity;
 import fi.fmi.avi.converter.tac.lexer.impl.FactoryBasedReconstructor;
-import fi.fmi.avi.converter.tac.lexer.impl.PrioritizedLexemeVisitor;
 import fi.fmi.avi.converter.tac.lexer.impl.ReconstructorContext;
 import fi.fmi.avi.converter.tac.lexer.impl.RegexMatchingLexemeVisitor;
 import fi.fmi.avi.converter.tac.lexer.impl.util.GeometryHelper;
 import fi.fmi.avi.model.AviationWeatherMessageOrCollection;
 import fi.fmi.avi.model.CircleByCenterPoint;
 import fi.fmi.avi.model.Geometry;
-import fi.fmi.avi.model.PolygonGeometry;
 import fi.fmi.avi.model.TacOrGeoGeometry;
-import fi.fmi.avi.model.bulletin.MeteorologicalBulletinSpecialCharacter;
 import fi.fmi.avi.model.sigmet.SIGMET;
 
-import java.util.ArrayList;
+import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
 import java.util.regex.Matcher;
 
+import static fi.fmi.avi.converter.tac.lexer.Lexeme.ParsedValueName.*;
 import static fi.fmi.avi.converter.tac.lexer.LexemeIdentity.SIGMET_WITHIN_RADIUS_OF_POINT;
 
-/**
- * Created by rinne on 10/02/17.
- */
 public class SigmetWithinRadius extends RegexMatchingLexemeVisitor {
     public SigmetWithinRadius(final OccurrenceFrequency prio) {
         super("^WI (\\d{2})(KM|NM) OF ((N|S)(\\d{2,4})) ((W|E)(\\d{3,5}))$", prio);
@@ -51,32 +41,32 @@ public class SigmetWithinRadius extends RegexMatchingLexemeVisitor {
 
         @Override
         public <T extends AviationWeatherMessageOrCollection> List<Lexeme> getAsLexemes(final T msg, Class<T> clz, final ReconstructorContext<T> ctx) {
-            List<Lexeme> lexemes = new ArrayList<>();
+            final ConversionHints hints = ctx.getHints();
+            final boolean specifyZeros = hints != null && hints.containsKey(ConversionHints.KEY_COORDINATE_MINUTES) &&
+                    ConversionHints.VALUE_COORDINATE_MINUTES_INCLUDE_ZERO.equals(hints.get(ConversionHints.KEY_COORDINATE_MINUTES));
             if (SIGMET.class.isAssignableFrom(clz)) {
+                final SIGMET sigmet = (SIGMET) msg;
                 final Optional<Integer> analysisIndex = ctx.getParameter("analysisIndex", Integer.class);
-                if (analysisIndex.isPresent()) {
-                    final TacOrGeoGeometry tacOrGeoGeometry = ((SIGMET) msg).getAnalysisGeometries().get().get(analysisIndex.get()).getGeometry().get();
-                    if (tacOrGeoGeometry.getGeoGeometry().isPresent()
-                            &&!tacOrGeoGeometry.getTacGeometry().isPresent()
-                            &&!tacOrGeoGeometry.getEntireArea()) {
-                        final Geometry geometry = tacOrGeoGeometry.getGeoGeometry().get();
-                        if (geometry instanceof CircleByCenterPoint) {
-                            double radius = ((CircleByCenterPoint)geometry).getRadius().getValue();
-                            String unit = ((CircleByCenterPoint)geometry).getRadius().getUom();
-                            List<Double> coords = ((CircleByCenterPoint)geometry).getCenterPointCoordinates();
-                            String tac = String.format(Locale.US, "WI %02.0f%s OF %s%04d %s%05d", radius, unit,
-                                    coords.get(0)<0?"S":"N", Math.round(coords.get(0)*100),
-                                    coords.get(1)<0?"W":"E", Math.round(coords.get(1)*100));
-                            lexemes.add(this.createLexeme(tac, LexemeIdentity.SIGMET_WITHIN_RADIUS_OF_POINT));
-                //            lexemes.add(this.createLexeme(MeteorologicalBulletinSpecialCharacter.SPACE.getContent(), LexemeIdentity.WHITE_SPACE));
+                if (analysisIndex.isPresent() && sigmet.getAnalysisGeometries().isPresent() && analysisIndex.get() < sigmet.getAnalysisGeometries().get().size()
+                        && sigmet.getAnalysisGeometries().get().get(analysisIndex.get()).getGeometry().isPresent()) {
+                    final TacOrGeoGeometry geom = sigmet.getAnalysisGeometries().get().get(analysisIndex.get()).getGeometry().get();
+                    if (geom.getGeoGeometry().isPresent() && !geom.getTacGeometry().isPresent() && !geom.getEntireArea()) {
+                        final Geometry geoGeometry = geom.getGeoGeometry().get();
+                        if (geoGeometry instanceof CircleByCenterPoint) {
+                            final CircleByCenterPoint circle = (CircleByCenterPoint) geoGeometry;
+                            final double radius = circle.getRadius().getValue();
+                            final String unit = circle.getRadius().getUom();
+                            final List<Double> coords = circle.getCenterPointCoordinates();
+                            final List<Lexeme> coordinateLexemes = GeometryHelper.getCoordinateString(BigDecimal.valueOf(coords.get(0)),
+                                    BigDecimal.valueOf(coords.get(1)), true, this::createLexeme, specifyZeros);
+                            final Lexeme circleLexeme = this.createLexeme(String.format(Locale.US, "WI %02.0f%s OF ", radius, unit) +
+                                    coordinateLexemes.get(0).getTACToken(), SIGMET_WITHIN_RADIUS_OF_POINT);
+                            return Collections.singletonList(circleLexeme);
                         }
-                    } else {
-                        // System.err.println("SIGMET_WITHIN recon has TACGeometry");
-                        // TODO should this condition be handled as error
                     }
                 }
             }
-            return lexemes;
+            return Collections.emptyList();
         }
     }
 }
