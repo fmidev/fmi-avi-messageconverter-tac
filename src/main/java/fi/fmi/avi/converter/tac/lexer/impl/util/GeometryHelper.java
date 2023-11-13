8d0ccdb144c0a55060762e9bb2d13efa7548e64b
kasarit <49980037+kasarit@users.noreply.github.com>
2023-11-13 18:45:59 +0200

    Fix SigmetWithinRadius coordinates (#187)

--- src/main/java/fi/fmi/avi/converter/tac/lexer/impl/util/GeometryHelper.java
+++ src/main/java/fi/fmi/avi/converter/tac/lexer/impl/util/GeometryHelper.java
@@ -1,26 +1,16 @@
 package fi.fmi.avi.converter.tac.lexer.impl.util;
 
+import fi.fmi.avi.converter.tac.lexer.Lexeme;
+import fi.fmi.avi.converter.tac.lexer.LexemeIdentity;
+import fi.fmi.avi.model.*;
+import fi.fmi.avi.model.bulletin.MeteorologicalBulletinSpecialCharacter;
+import fi.fmi.avi.model.immutable.PointGeometryImpl;
+
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Locale;
-import java.util.Set;
+import java.util.*;
 import java.util.function.BiFunction;
-
-import fi.fmi.avi.converter.tac.lexer.Lexeme;
-import fi.fmi.avi.converter.tac.lexer.LexemeIdentity;
-import fi.fmi.avi.model.CoordinateReferenceSystem;
-import fi.fmi.avi.model.Geometry;
-import fi.fmi.avi.model.PointGeometry;
-import fi.fmi.avi.model.PolygonGeometry;
-import fi.fmi.avi.model.Winding;
-import fi.fmi.avi.model.bulletin.MeteorologicalBulletinSpecialCharacter;
-import fi.fmi.avi.model.immutable.PointGeometryImpl;
 
 public class GeometryHelper {
     private static final Set<String> LATITUDE_AXIS_LABELS = Collections.unmodifiableSet(new HashSet<>(Arrays.asList("lat", "latitude")));
@@ -54,9 +44,8 @@ public class GeometryHelper {
             if ((specifyZeros || lonDecimalPart.compareTo(BigDecimal.ZERO) != 0)) {
                 lonBuilder.append(String.format(Locale.US, "%02.0f", lonDecimalPart.abs().multiply(BigDecimal.valueOf(60d)).round(mc)));
             }
-            lexemes.add(createLexeme.apply(
-                    latBuilder.toString() + MeteorologicalBulletinSpecialCharacter.SPACE.getContent()
-                            + lonBuilder.toString(), LexemeIdentity.POLYGON_COORDINATE_PAIR));
+            lexemes.add(createLexeme.apply(latBuilder + MeteorologicalBulletinSpecialCharacter.SPACE.getContent()
+                    + lonBuilder, LexemeIdentity.POLYGON_COORDINATE_PAIR));
             if (!lastPair) {
                 lexemes.add(createLexeme.apply(MeteorologicalBulletinSpecialCharacter.SPACE.getContent(),
                         LexemeIdentity.WHITE_SPACE));
@@ -65,37 +54,40 @@ public class GeometryHelper {
                         LexemeIdentity.WHITE_SPACE));
             }
         } else {
-
+            // TODO
         }
         return lexemes;
     }
 
     public static List<Lexeme> getGeoLexemes(Geometry geom, BiFunction<String, LexemeIdentity, Lexeme> createLexeme) {
-        return  getGeoLexemes(geom, createLexeme, false);
+        return getGeoLexemes(geom, createLexeme, false);
     }
 
     public static List<Lexeme> getGeoLexemes(Geometry geom, BiFunction<String, LexemeIdentity, Lexeme> createLexeme, boolean specifyZeros) {
-        List<Lexeme> lexemes = new ArrayList<>();
+        final List<Lexeme> lexemes = new ArrayList<>();
+
+        //Add check for WGS84 lat, lon CRS, EPSG:4326 or variants of the ID?
+        int latOffset = -1;
+        int lonOffset = -1;
+        final List<String> axisLabels = geom.getCrs().map(CoordinateReferenceSystem::getAxisLabels).orElse(Collections.emptyList());
+        for (int i = axisLabels.size() - 1; i >= 0; i--) {
+            final String axisLabel = axisLabels.get(i).toLowerCase(Locale.US);
+            if (LATITUDE_AXIS_LABELS.contains(axisLabel)) {
+                latOffset = i;
+            } else if (LONGITUDE_AXIS_LABELS.contains(axisLabel)) {
+                lonOffset = i;
+            }
+        }
+        //defaults to EPSG:4326 (lat,lon) order:
+        if (latOffset == -1) {
+            latOffset = 0;
+        }
+        if (lonOffset == -1) {
+            lonOffset = 1;
+        }
+
         if (geom instanceof PolygonGeometry) {
             //Add check for WGS84 lat, lon CRS, EPSG:4326 or variants of the ID?
-            int latOffset = -1;
-            int lonOffset = -1;
-            final List<String> axisLabels = geom.getCrs().map(CoordinateReferenceSystem::getAxisLabels).orElse(Collections.emptyList());
-            for (int i = axisLabels.size() - 1; i >= 0; i--) {
-                final String axisLabel = axisLabels.get(i).toLowerCase(Locale.US);
-                if (LATITUDE_AXIS_LABELS.contains(axisLabel)) {
-                    latOffset = i;
-                } else if (LONGITUDE_AXIS_LABELS.contains(axisLabel)) {
-                    lonOffset = i;
-                }
-            }
-            //defaults to EPSG:4326 (lat,lon) order:
-            if (latOffset == -1) {
-                latOffset = 0;
-            }
-            if (lonOffset == -1) {
-                lonOffset = 1;
-            }
             final List<Double> coords = ((PolygonGeometry) geom).getExteriorRingPositions(Winding.CLOCKWISE);
             int latIndex;
             int lonIndex;
@@ -107,25 +99,6 @@ public class GeometryHelper {
                 lexemes.addAll(getCoordinateString(lat, lon, (coordPairIndex >= coords.size() - 2), createLexeme, specifyZeros));
             }
         } else if (geom instanceof PointGeometry) {
-            //Add check for WGS84 lat, lon CRS, EPSG:4326 or variants of the ID?
-            int latOffset = -1;
-            int lonOffset = -1;
-            final List<String> axisLabels = geom.getCrs().map(CoordinateReferenceSystem::getAxisLabels).orElse(Collections.emptyList());
-            for (int i = axisLabels.size() - 1; i >= 0; i--) {
-                final String axisLabel = axisLabels.get(i).toLowerCase(Locale.US);
-                if (LATITUDE_AXIS_LABELS.contains(axisLabel)) {
-                    latOffset = i;
-                } else if (LONGITUDE_AXIS_LABELS.contains(axisLabel)) {
-                    lonOffset = i;
-                }
-            }
-            //defaults to EPSG:4326 (lat,lon) order:
-            if (latOffset == -1) {
-                latOffset = 0;
-            }
-            if (lonOffset == -1) {
-                lonOffset = 1;
-            }
             final List<Double> coords = ((PointGeometry) geom).getCoordinates();
             final BigDecimal lat = BigDecimal.valueOf(coords.get(latOffset)).setScale(2, RoundingMode.HALF_UP);
             final BigDecimal lon = BigDecimal.valueOf(coords.get(lonOffset)).setScale(2, RoundingMode.HALF_UP);
@@ -139,7 +112,7 @@ public class GeometryHelper {
         double latitude;
         double longitude;
         if (latStr.length() > 3) {
-            double latitudeMinutes = Double.parseDouble(latStr.substring(3))/60.;
+            double latitudeMinutes = Double.parseDouble(latStr.substring(3)) / 60.;
             latitude = Double.parseDouble(latStr.substring(1, 3) + ".") + latitudeMinutes;
         } else {
             latitude = Double.parseDouble(latStr.substring(1));
@@ -148,7 +121,7 @@ public class GeometryHelper {
             latitude *= -1;
         }
         if (lonStr.length() > 4) {
-            double longitudeMinutes = Double.parseDouble(lonStr.substring(4))/60.;
+            double longitudeMinutes = Double.parseDouble(lonStr.substring(4)) / 60.;
             longitude = Double.parseDouble(lonStr.substring(1, 4) + ".") + longitudeMinutes;
         } else {
             longitude = Double.parseDouble(lonStr.substring(1));
@@ -160,43 +133,6 @@ public class GeometryHelper {
         pointBuilder.addCoordinates(latitude, longitude);
         return pointBuilder.build();
 
-    }
-    public class Point {
-        final double latitude;
-        final double longitude;
-        public Point(String latStr, String lonStr) {
-            double lat;
-            double lon;
-            if (latStr.length() > 3) {
-                double latitudeMinutes = Double.parseDouble(latStr.substring(3))/60.;
-                lat = Double.parseDouble(latStr.substring(1, 3) + ".") + latitudeMinutes;
-            } else {
-                lat = Double.parseDouble(latStr.substring(1));
-            }
-            if (latStr.charAt(0) == 'S') {
-                latitude = lat * -1;
-            } else {
-                latitude=lat;
-            }
-            if (lonStr.length() > 4) {
-                double longitudeMinutes = Double.parseDouble(lonStr.substring(4))/60.;
-                lon = Double.parseDouble(lonStr.substring(1, 4) + ".") + longitudeMinutes;
-            } else {
-                lon = Double.parseDouble(lonStr.substring(1));
-            }
-            if (lonStr.charAt(0) == 'W') {
-                longitude = lon * -1;
-            } else {
-                longitude = lon;
-            }
-        }
-        public double getLongitude() {
-            return longitude;
-        }
-
-        public double getLatitude() {
-            return latitude;
-        }
     }
 
 }
